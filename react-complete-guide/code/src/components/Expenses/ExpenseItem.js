85b15a8f4f7e5971d32dede78805621f9750eb7f
Jacob Reese <jacobreese123@gmail.com>
2021-10-15 19:55:16 -0400

    create button to update title of expenses

--- react-complete-guide/code/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/code/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,25 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate.js";
 import Card from "../UI/Card"
+import React, {useState} from 'react';
 
 const ExpenseItem = (props) => {
+
+  let title = props.title;
+
+  const clickHandler = () => {
+    title = 'Updated!'
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </Card>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>-
   );
 }
 
