b840072680359a9e0d9d61b5a3df2251843925e8
Jacob Reese <jacobreese123@gmail.com>
2021-10-17 09:26:46 -0400

    create form handlers for adding new expenses and add variables to hold those values

--- react-complete-guide/code/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/code/src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,85 @@
 import "./ExpenseForm.css";
+import { useState } from "react";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState(""); // option 1
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  //   const [userInput, setUserInput] = useState({           // option 2 + 3
+  //     enteredTitle: "",
+  //     enteredAmount: "",
+  //     enteredDate: "",
+  //   });
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value); // option 1
+    // setUserInput({                       // option 2
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+    //   setUserInput((prevState) => {                                     //option 3 - best option if you depend on previous state
+    //       return { ...prevState, enteredTitle: event.target.value };
+    //   });
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value); // option 1
+    // setUserInput({                           // option 2
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
+    // setUserInput((prevState) => {                                     //option 3 - best option if you depend on previous state
+    //     return { ...prevState, enteredAmount: event.target.value };
+    // });
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value); // option 1
+    // setUserInput({                       // option 2
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
+    // setUserInput((prevState) => {                                     //option 3 - best option if you depend on previous state
+    //     return { ...prevState, enteredDate: event.target.value };
+    // });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
