11343c4639a941f087c84ff25f55f5bf4a7ddf83
Jacob Reese <jacobreese123@gmail.com>
2021-10-24 17:20:45 -0400

    create listing feature for users

--- react-complete-guide/code/src/App.js
+++ react-complete-guide/code/src/App.js
@@ -1,13 +1,22 @@
 import AddUser from './Components/Users/AddUser';
-import React from 'react';
+import React, {useState} from 'react';
 import UserList from './Components/Users/UserList';
 
 
+
 function App() {
+  const [userList, setUserList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUserList((prevUsersList) => {
+      return [...prevUsersList, { name: uName, age: uAge, id: Math.random().toString()}];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
-      <UserList user={[]} />
+      <AddUser onAddUser={addUserHandler} />
+      <UserList user={userList} />
     </div>
   );
 }
