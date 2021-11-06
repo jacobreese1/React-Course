882411dd9a971d116f0ca3752468795d5f0d5a2b
Jacob Reese <jacobreese123@gmail.com>
2021-11-05 22:28:43 -0400

    add counter functionality

--- react-complete-guide/code/src/App.js
+++ react-complete-guide/code/src/App.js
@@ -1,12 +1,13 @@
-import { useState } from "react";
-import Header from "./Components/Layout/Header";
-import Meals from "./Components/Meals/Meals";
-import Cart from "./Components/Cart/Cart";
-import CartProvider from "./Store/CartProvider";
+import { useState } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
-  
+
   const showCartHandler = () => {
     setCartIsShown(true);
   };
@@ -18,7 +19,7 @@ function App() {
   return (
     <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler}/>
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
--- react-complete-guide/code/src/Components/Layout/HeaderCartButton.js
+++ react-complete-guide/code/src/Components/Layout/HeaderCartButton.js
@@ -1,7 +1,8 @@
-import classes from "./HeaderCartButton.module.css";
-import CartIcon from "../Cart/CartIcon";
-import CartContext from "../../Store/cart-context";
-import { useContext } from "react";
+import { useContext } from 'react';
+
+import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
+import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
