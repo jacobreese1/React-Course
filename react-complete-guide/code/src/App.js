8b8d80b6fbc104e6638e554be24e29c2a1384488
Jacob Reese <jacobreese123@gmail.com>
2021-11-11 17:54:00 -0500

    add initial files

--- react-complete-guide/code/src/App.js
+++ react-complete-guide/code/src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
-
+import Users from './components/Users';
 
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Users />
     </div>
   );
 }
