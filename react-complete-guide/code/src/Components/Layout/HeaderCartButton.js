3f814a2c44ebc9453b006e8b2feacca0317e2782
Jacob Reese <jacobreese123@gmail.com>
2021-11-20 17:24:42 -0500

    initialize files

--- react-complete-guide/code/src/Components/Layout/HeaderCartButton.js
+++ react-complete-guide/code/src/Components/Layout/HeaderCartButton.js
@@ -1,8 +1,8 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from 'react';
 
-import CartIcon from "../Cart/CartIcon";
-import CartContext from "../../store/cart-context";
-import classes from "./HeaderCartButton.module.css";
+import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
+import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
   const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
@@ -10,16 +10,14 @@ const HeaderCartButton = (props) => {
 
   const { items } = cartCtx;
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
-  const btnClasses = `${classes.button} ${
-    btnIsHighlighted ? classes.bump : ""
-  }`;
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
 
   useEffect(() => {
-    if (cartCtx.items.length === 0) {
+    if (items.length === 0) {
       return;
     }
     setBtnIsHighlighted(true);
