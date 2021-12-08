fa037356e1fb4f4f3d09ea8f2654c94678faa8ed
Jacob Reese <jacobreese123@gmail.com>
2021-12-07 19:52:22 -0500

    create initial routes for all-quotes, new-quotes, and quote-details

--- react-complete-guide/code/src/App.js
+++ react-complete-guide/code/src/App.js
@@ -1,7 +1,22 @@
+import { Switch, Route } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import QuoteDetail from "./pages/QuoteDetail";
+import NewQuote from "./pages/NewQuote";
+
 function App() {
   return (
     <div>
-
+      <Switch>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteID">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
     </div>
   );
 }
