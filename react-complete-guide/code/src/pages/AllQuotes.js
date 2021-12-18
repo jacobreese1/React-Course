53c0f54dce914d721740f866feda18d0e7ad470e
Jacob Reese <jacobreese123@gmail.com>
2021-12-17 19:10:32 -0500

    write more flexible routing

--- react-complete-guide/code/src/pages/AllQuotes.js
+++ react-complete-guide/code/src/pages/AllQuotes.js
@@ -1,30 +1,12 @@
-import QuoteList from "../components/quotes/QuoteList";
+import QuoteList from '../components/quotes/QuoteList';
 
 const DUMMY_QUOTES = [
-    {
-        id: 'q1',
-        author: "Nelson Mandela",
-        text: "It always seems impossible until it's done.",
-    },
-    {
-        id: 'q2',
-        author: "Ken Venturi",
-        text: "I don't believe you have to be better than everybody else. I believe you have to be better than you ever thought you could be.",
-    },
-    {
-        id: 'q3',
-        author: "George S. Patton",
-        text: "A good plan violently executed now is better than a perfect plan executed next week.",
-    },
-    {
-        id: 'q4',
-        author: "Babe Ruth",
-        text: "You just can't beat the person who never gives up.",
-    },
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
 ];
 
 const AllQuotes = () => {
-    return <QuoteList quotes={DUMMY_QUOTES}/>
- };
+  return <QuoteList quotes={DUMMY_QUOTES} />
+};
 
 export default AllQuotes;
--- react-complete-guide/code/src/pages/QuoteDetail.js
+++ react-complete-guide/code/src/pages/QuoteDetail.js
@@ -1,29 +1,12 @@
-import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
-import { Fragment } from "react";
-import Comments from "../components/comments/Comments";
-import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import { Fragment } from 'react';
+import { useParams, Route, Link, useRouteMatch } from 'react-router-dom';
+
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import Comments from '../components/comments/Comments';
 
 const DUMMY_QUOTES = [
-  {
-    id: "q1",
-    author: "Nelson Mandela",
-    text: "It always seems impossible until it's done.",
-  },
-  {
-    id: "q2",
-    author: "Ken Venturi",
-    text: "I don't believe you have to be better than everybody else. I believe you have to be better than you ever thought you could be.",
-  },
-  {
-    id: "q3",
-    author: "George S. Patton",
-    text: "A good plan violently executed now is better than a perfect plan executed next week.",
-  },
-  {
-    id: "q4",
-    author: "Babe Ruth",
-    text: "You just can't beat the person who never gives up.",
-  },
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
 ];
 
 const QuoteDetail = () => {
@@ -39,14 +22,14 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={match.url} exact>
-        <div className="centered">
-          <Link className="btn--flat" to={`${match.url}/comments`}>
+      <Route path={match.path} exact>
+        <div className='centered'>
+          <Link className='btn--flat' to={`${match.url}/comments`}>
             Load Comments
           </Link>
         </div>
       </Route>
-      <Route path={`${match.url}/comments`}>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
