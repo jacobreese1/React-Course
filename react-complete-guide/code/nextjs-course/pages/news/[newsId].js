9fb7b5bd2f7ca27d9275ade8d99fd3bf01a33fd6
Jacob Reese <jacobreese123@gmail.com>
2022-01-08 18:51:23 -0500

    add links between pages to utilize the dynamic ability of the [newsId].js page

--- react-complete-guide/code/nextjs-course/pages/news/[newsId].js
+++ react-complete-guide/code/nextjs-course/pages/news/[newsId].js
@@ -1,6 +1,15 @@
+import { useRouter } from "next/router";
+
 // our-domain.com/news/detail
 
 const DetailPage = () => {
+    const router = useRouter();
+
+    const newsId = router.query.newsId;
+
+    //send a request to backend API
+    //to fetch the news item with newsId
+
     return <h1>The Detail Page</h1>;
 };
 
--- react-complete-guide/code/nextjs-course/pages/news/index.js
+++ react-complete-guide/code/nextjs-course/pages/news/index.js
@@ -1,7 +1,22 @@
+import { Fragment } from "react";
+import Link from "next/link";
+
 // our-domain.com/news
 
 const NewsPage = () => {
-     return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS Is A Great Framework
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
+  );
 };
 
 export default NewsPage;
