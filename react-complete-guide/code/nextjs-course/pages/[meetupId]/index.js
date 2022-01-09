e822db862d67a002eb0f3a3373019d663e906ff5
Jacob Reese <jacobreese123@gmail.com>
2022-01-08 19:46:09 -0500

    create new detail component and add new styling

---
+++ react-complete-guide/code/nextjs-course/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
+      title="A First Meetup"
+      address="123 Fresh Prince Rd"
+      description="Meetup description"
+    />
+  );
+};
+
+export default MeetupDetails;
