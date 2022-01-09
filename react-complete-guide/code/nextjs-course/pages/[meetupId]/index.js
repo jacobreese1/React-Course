18aeb666ef2802856afbea0160ab00e17848675c
Jacob Reese <jacobreese123@gmail.com>
2022-01-08 20:57:51 -0500

    add static props and paths to meetupId index

--- react-complete-guide/code/nextjs-course/pages/[meetupId]/index.js
+++ react-complete-guide/code/nextjs-course/pages/[meetupId]/index.js
@@ -11,4 +11,40 @@ const MeetupDetails = () => {
   );
 };
 
+export const getStaticPaths = async () => {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+};
+
+export const getStaticProps = async (context) => {
+  //fetch data for a single meetup
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "A First Meetup",
+        address: "123 Fresh Prince Rd",
+        description: "Meetup description",
+      },
+    },
+  };
+};
+
 export default MeetupDetails;
