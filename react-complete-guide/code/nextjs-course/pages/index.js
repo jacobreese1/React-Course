9b45dac25a540a981461f664c5d25494b52a6c93
Jacob Reese <jacobreese123@gmail.com>
2022-01-08 20:07:11 -0500

    prerender page using getStaticProps

--- react-complete-guide/code/nextjs-course/pages/index.js
+++ react-complete-guide/code/nextjs-course/pages/index.js
@@ -1,32 +1,35 @@
-import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
-    {
-        id: "m1",
-        title: "First Meetup",
-        image: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
-        address: "123 Fresh Prince Rd",
-        description: "The First Meetup"
-    },
-    {
-        id: "m2",
-        title: "Second Meetup",
-        image: "https://upload.wikimedia.org/wikipedia/commons/5/52/Churchill_Downs_Entrance_Barbaro_Statue.jpg",
-        address: "123 Broken Bad Rd",
-        description: "The Second Meetup"
-    }
+  {
+    id: "m1",
+    title: "First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "123 Fresh Prince Rd",
+    description: "The First Meetup",
+  },
+  {
+    id: "m2",
+    title: "Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/5/52/Churchill_Downs_Entrance_Barbaro_Statue.jpg",
+    address: "123 Broken Bad Rd",
+    description: "The Second Meetup",
+  },
 ];
 
-const HomePage = () => {
-    const [ loadedMeetups, setLoadedMeetups ] = useState([]);
-
-    useEffect(() => {
-        //send a http request and fetch data
-        setLoadedMeetups(DUMMY_MEETUPS);
-    }, []);
-
-    return <MeetupList meetups={loadedMeetups} />;
+const HomePage = (props) => {
+  return <MeetupList meetups={props.meetups} />;
 };
+
+export async function getStaticProps() {
+  //fetch data from API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
+}
 
 export default HomePage;
