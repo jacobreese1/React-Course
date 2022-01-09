5cffd9cee9accc4f185ff89295f8186878d48134
Jacob Reese <jacobreese123@gmail.com>
2022-01-08 19:07:19 -0500

    create dummy list of meetups and display on webpage

--- react-complete-guide/code/nextjs-course/pages/index.js
+++ react-complete-guide/code/nextjs-course/pages/index.js
@@ -1,5 +1,24 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+    {
+        id: "m1",
+        title: "First Meetup",
+        image: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+        address: "123 Fresh Prince Rd",
+        description: "The First Meetup"
+    },
+    {
+        id: "m2",
+        title: "Second Meetup",
+        image: "https://upload.wikimedia.org/wikipedia/commons/5/52/Churchill_Downs_Entrance_Barbaro_Statue.jpg",
+        address: "123 Broken Bad Rd",
+        description: "The Second Meetup"
+    }
+];
+
 const HomePage = () => {
-    return <h1>Home Page</h1>
-}
+    return <MeetupList meetups={DUMMY_MEETUPS} />;
+};
 
 export default HomePage;
