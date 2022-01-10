3dfceb9de4658e5ba3eb6ecfe8f574abe59c7514
Jacob Reese <jacobreese123@gmail.com>
2022-01-10 17:24:49 -0500

    send http requests

--- react-complete-guide/code/nextjs-course/pages/api/new-meetup.js
+++ react-complete-guide/code/nextjs-course/pages/api/new-meetup.js
@@ -7,18 +7,18 @@ const handler = async (req, res) => {
   if (req.method === "POST") {
     const data = req.body;
 
-    const client = MongoClient.connect(
+    const client = await MongoClient.connect(
       "mongodb+srv://jreese:Coding21@cluster0.w2wpc.mongodb.net/meetups?retryWrites=true&w=majority"
     );
     const db = client.db();
 
     const meetupsCollection = db.collection("meetups");
 
-    const result = await meetupsCollection.insertOne({ data });
+    const result = await meetupsCollection.insertOne(data);
 
     console.log(result);
 
-    (await client).close();
+    client.close();
 
     res.status(201).json({ message: "Meetup inserted!" });
   }
