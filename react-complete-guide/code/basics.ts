e065b71ed2b858bfc761a7c1bd1e7d63727127e6
Jacob Reese <jacobreese123@gmail.com>
2022-02-05 16:46:02 -0500

    add basics.ts file to learn initial typescript formatting

---
+++ react-complete-guide/code/basics.ts
@@ -0,0 +1,17 @@
+// Primitives: number, string, boolean
+// Complex: arrays, objects
+// Function types: parameters
+
+//Primitives
+
+let age: number;
+
+age = 12;
+
+let userName: string;
+
+userName = "Vivisepulture";
+
+let isStudent: boolean;
+
+isStudent = true;
